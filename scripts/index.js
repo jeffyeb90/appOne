b4d10d4e7f9d91fed10168e7b413604b7814060c
Jeffrey Takyi-Yeboah <jeffrey.takyi-yeboah@ashesi.edu.gh>
2016-10-16 23:14:48 +0000

    Changes
    Changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-﻿(function() {
+(function() {
 
 	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 	var pictureSource;
@@ -8,6 +8,7 @@
 		destinationType = navigator.camera.DestinationType;
 
 		document.getElementById("capturePhoto").onclick = function() {
+			//alert("here");
 			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
 				quality : 50,
 
@@ -18,6 +19,7 @@
 
 
 	};
+
 	function onPhotoDataSuccess(imageData) {
 
 		var smallImage = document.getElementById('smallImage');
@@ -33,24 +35,43 @@
 		alert('Failed because: ' + message);
 
 	}
-	function Scanner(){
-	cordova.plugins.barcodeScanner.scan(
-		function (result) {
-				alert("We got a barcode\n" +
-							"Result: " + result.text + "\n" +
-							"Format: " + result.format + "\n" +
-							"Cancelled: " + result.cancelled);
-		},
-		function (error) {
-				alert("Scanning failed: " + error);
-		},
-		{
-				"preferFrontCamera" : true, // iOS and Android
-				"showFlipCameraButton" : true, // iOS and Android
-				"prompt" : "Place a barcode inside the scan area", // supported on Android only
-				"formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-				"orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
-		}
- );
 
+
+})();
+
+
+(function() {
+
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+	function onDeviceReady() {
+
+
+		document.getElementById("barcodeScanner").onclick = function barcode(){
+			//alert("here");
+			cordova.plugins.barcodeScanner.scan(
+		 function (result) {
+				 alert("We got a barcode\n" +
+							 "Result: " + result.text + "\n" +
+							 "Format: " + result.format + "\n" +
+							 "Cancelled: " + result.cancelled);
+		 },
+		 function (error) {
+				 alert("Scanning failed: " + error);
+		 },
+		 {
+				 "preferFrontCamera" : true, // iOS and Android
+				 "showFlipCameraButton" : true, // iOS and Android
+				 "prompt" : "Place a barcode inside the scan area", // supported on Android only
+				 "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+				 "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
+		 }
+	);
+
+
+
+	};
+
+
+
+}
 })();
