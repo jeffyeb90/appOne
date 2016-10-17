522e1b83afb58dc80f41092b4a0b5b7f764eef4f
Jeffrey Takyi-Yeboah <jeffrey.takyi-yeboah@ashesi.edu.gh>
2016-10-17 00:23:04 +0000

    Changes
    change

--- js/app/index.js
+++ js/app/index.js
@@ -1,77 +1,20 @@
-﻿(function() {
+(function camera() {
 
-	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-	var pictureSource;
-	var destinationType;
-	function onDeviceReady() {
-		pictureSource = navigator.camera.PictureSourceType;
-		destinationType = navigator.camera.DestinationType;
-
-		document.getElementById("capturePhoto").onclick = function() {
-			//alert("here");
-			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-				quality : 50,
-
-				destinationType : destinationType.DATA_URL
-			});
-		}
-
-
-
-	};
-
-	function onPhotoDataSuccess(imageData) {
-
-		var smallImage = document.getElementById('smallImage');
-
-		smallImage.style.display = 'block';
-
-		smallImage.src = "data:image/jpeg;base64," + imageData;
-
-	}
-
-	function onFail(message) {
-
-		alert('Failed because: ' + message);
-
-	}
-
-
-})();
-
-
-(function() {
-
-	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-	function onDeviceReady() {
-
-
-		document.getElementById("barcodeScanner").onclick = function barcode(){
-			//alert("here");
-			cordova.plugins.barcodeScanner.scan(
-		 function (result) {
-				 alert("We got a barcode\n" +
-							 "Result: " + result.text + "\n" +
-							 "Format: " + result.format + "\n" +
-							 "Cancelled: " + result.cancelled);
-		 },
-		 function (error) {
-				 alert("Scanning failed: " + error);
-		 },
-		 {
-				 "preferFrontCamera" : true, // iOS and Android
-				 "showFlipCameraButton" : true, // iOS and Android
-				 "prompt" : "Place a barcode inside the scan area", // supported on Android only
-				 "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-				 "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
-		 }
-	);
-
-
-
-	};
-
-
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    console.log(navigator.camera);
 }
 
-})();
+
+navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+    destinationType: Camera.DestinationType.FILE_URI });
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('myImage');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+};
