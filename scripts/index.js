65b8019a02d00f5fd690052cdfda26b91ebaef05
Jeffrey Takyi-Yeboah <jeffrey.takyi-yeboah@ashesi.edu.gh>
2016-10-25 18:13:09 +0000

    New
    changes

--- scripts/index.js
+++ scripts/index.js
@@ -41,11 +41,6 @@
 
 (function() {
 
-//barcode scanner
-
-	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-	function onDeviceReady() {
-
 
 		document.getElementById("barcodeScanner").onclick = function(){
 			//alert("here");
@@ -71,7 +66,7 @@
 
 
 	}
-	};
+
 
 	})();
 
@@ -83,7 +78,7 @@
 
 
 	document.getElementById("geolocationdata").onclick = function(){
-		alert("here!");
+		 //alert("here!");
 		 navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 6000});
 	}
 
@@ -91,10 +86,6 @@
 	var onSuccess = function(position) {
 		 alert('Latitude: '          + position.coords.latitude          + '\n' +
 					 'Longitude: '         + position.coords.longitude         + '\n' +
-					 'Altitude: '          + position.coords.altitude          + '\n' +
-					 'Accuracy: '          + position.coords.accuracy          + '\n' +
-					 'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-					 'Heading: '           + position.coords.heading           + '\n' +
 					 'Speed: '             + position.coords.speed             + '\n' +
 					 'Timestamp: '         + position.timestamp                + '\n');
 
@@ -122,7 +113,7 @@ google.maps.event.addDomListener(window, 'load', onSuccess);
 
 
 		document.getElementById("geolocationdatawatch").onclick = function(){
-			alert("here!");
+			//alert("here!");
 			 navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 60000});
 		}
 
