c4b9174742ab0a8efc92bacfb262fab9404e4673
Jeffrey Takyi-Yeboah <jeffrey.takyi-yeboah@ashesi.edu.gh>
2016-11-19 01:07:46 +0000

    commit
    commits

--- scripts/index.js
+++ scripts/index.js
@@ -1,111 +1,147 @@
+﻿(function() {
+
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+	var pictureSource;
+	var destinationType;
+	function onDeviceReady() {
+		pictureSource = navigator.camera.PictureSourceType;
+		destinationType = navigator.camera.DestinationType;
+
+		document.getElementById("capturePhoto").onclick = function() {
+			//alert("here");
+			navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
+				quality : 50,
+
+				destinationType : destinationType.DATA_URL
+			});
+		}
+
+
+
+	};
+
+	function onPhotoDataSuccess(imageData) {
+
+		var smallImage = document.getElementById('smallImage');
+
+		smallImage.style.display = 'block';
+
+		smallImage.src = "data:image/jpeg;base64," + imageData;
+
+	}
+
+	function onFail(message) {
+
+		alert('Failed because: ' + message);
+
+	}
+
+	})();
+
+
 (function() {
 
-document.addEventListener('deviceready', onDeviceReady.bind(this), false);
-var pictureSource;
-var destinationType;
-function onDeviceReady() {
-pictureSource = navigator.camera.PictureSourceType;
-destinationType = navigator.camera.DestinationType;
+//barcode scanner
 
-document.getElementById("capturePhoto").onclick = function() {
-navigator.camera.getPicture(onPhotoDataSuccess, onFail, {
-quality : 50,
-
-destinationType : destinationType.DATA_URL
-});
-}
-
-document.getElementById("geolocationdata").addEventListener("click", function() {
-navigator.geolocation.getCurrentPosition(onSuccess, onError, {
-enableHighAccuracy : true
-});
-});
-
-//watchPosition
-var watchId = navigator.geolocation.watchPosition(onWatchSuccess, onWatchError, {
-timeout : 30000
-});
-
-document.getElementById("clearWatchbtn").addEventListener("click", function() {
-navigator.geolocation.clearWatch(watchID);
-});
-
-document.getElementById('barcode').onclick=function(){
-cordova.plugins.barcodeScanner.scan(onBarcodeSuccess,onBarcodeFail
-,
+	document.addEventListener('deviceready', onDeviceReady.bind(this), false);
+	function onDeviceReady() {
 
 
-{
-         "preferFrontCamera" : true, // iOS and Android
-         "showFlipCameraButton" : true, // iOS and Android
-         "prompt" : "Place a barcode inside the scan area", // supported on Android only
-         "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-         "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
+		document.getElementById("barcodeScanner").onclick = function(){
+			//alert("here");
+			cordova.plugins.barcodeScanner.scan(
+		 function (result) {
+				 alert("We got a barcode\n" +
+							 "Result: " + result.text + "\n" +
+							 "Format: " + result.format + "\n" +
+							 "Cancelled: " + result.cancelled);
+		 },
+		 function (error) {
+				 alert("Scanning failed: " + error);
+		 },
+		 {
+				 "preferFrontCamera" : true, // iOS and Android
+				 "showFlipCameraButton" : true, // iOS and Android
+				 "prompt" : "Place a barcode inside the scan area", // supported on Android only
+				 "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+				 "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
+		 }
+	);
 
-});
-}
-
-// var mapCanvas = document.getElementById("map");
-  //
-var mapOptions = {
-//    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
-//  };
-//  var map = new google.maps.Map(mapCanvas, mapOptions);
-
-};
 
 
-function onMapInit(map) {
-}
+	}
+	};
 
-function onPhotoDataSuccess(imageData) {
+	})();
 
-var smallImage = document.getElementById('smallImage');
 
-smallImage.style.display = 'block';
 
-smallImage.src = "data:image/jpeg;base64," + imageData;
 
-}
 
-function onFail(message) {
+(function() {
 
-alert('Failed because: ' + message);
 
-}
+	document.getElementById("geolocationdata").onclick = function(){
+		alert("here!");
+		 navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 6000});
+	}
 
-///////////geolocation bit/////////////////
-var onSuccess = function(position) {
-alert('Latitude: ' + position.coords.latitude + '\n' + 'Longitude: ' + position.coords.longitude + '\n');
-};
 
-// onError Callback receives a PositionError object
-//
-function onError(error) {
-alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-}
+	var onSuccess = function(position) {
+		 alert('Latitude: '          + position.coords.latitude          + '\n' +
+					 'Longitude: '         + position.coords.longitude         + '\n' +
+					 'Altitude: '          + position.coords.altitude          + '\n' +
+					 'Accuracy: '          + position.coords.accuracy          + '\n' +
+					 'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+					 'Heading: '           + position.coords.heading           + '\n' +
+					 'Speed: '             + position.coords.speed             + '\n' +
+					 'Timestamp: '         + position.timestamp                + '\n');
 
-//watchPosition
+ };
 
-var onWatchSuccess = function(position) {
-var element = document.getElementById('divWatchMeMove');
-element.innerHTML = 'Latitude: ' + position.coords.latitude + '<br />' + 'Longitude: ' + position.coords.longitude + '<br />' + '<hr />' + element.innerHTML;
-};
+ // onError Callback receives a PositionError object
+ //
+ function onError(error) {
+		 alert('code: '    + error.code    + '\n' +
+					 'message: ' + error.message + '\n');
+ }
+google.maps.event.addDomListener(window, 'load', onSuccess);
 
-function onWatchError(error) {
-alert('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
-}
 
-function onBarcodeSuccess(result) {
-         alert("We got a barcode\n" +
-               "Result: " + result.text + "\n" +
-               "Format: " + result.format + "\n" +
-               "Cancelled: " + result.cancelled);
-}
+})();
 
-function onBarcodeFail(error) {
-         alert("Scanning failed: " + error);
 
-}
+
+
+
+
+(function() {
+
+
+
+
+		document.getElementById("geolocationdatawatch").onclick = function(){
+			alert("here!");
+			 navigator.geolocation.watchPosition(onSuccess, onError, { timeout: 60000});
+		}
+
+
+		var onSuccess = function(position) {
+			 alert('Latitude: '          + position.coords.latitude          + '\n' +
+						 'Longitude: '         + position.coords.longitude         + '\n');
+
+
+	 };
+
+	 // onError Callback receives a PositionError object
+	 //
+	 function onError(error) {
+			 alert('code: '    + error.code    + '\n' +
+						 'message: ' + error.message + '\n');
+	 }
+
+
+
 
 })();
