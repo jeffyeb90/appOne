e85c9cc901f0dbb986935c7326048d75b6ef87f0
Jeffrey Takyi-Yeboah <jeffrey.takyi-yeboah@ashesi.edu.gh>
2016-11-20 23:20:50 +0000

    new changes
    Works

--- scripts/index.js
+++ scripts/index.js
@@ -83,19 +83,15 @@
 
 
 	document.getElementById("geolocationdata").onclick = function(){
-		alert("here!");
-		 navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 6000});
+		 navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 2000});
 	}
-
 
 	var onSuccess = function(position) {
 		 alert('Latitude: '          + position.coords.latitude          + '\n' +
 					 'Longitude: '         + position.coords.longitude         + '\n' +
-					 'Altitude: '          + position.coords.altitude          + '\n' +
+
 					 'Accuracy: '          + position.coords.accuracy          + '\n' +
-					 'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-					 'Heading: '           + position.coords.heading           + '\n' +
-					 'Speed: '             + position.coords.speed             + '\n' +
+
 					 'Timestamp: '         + position.timestamp                + '\n');
 
  };
